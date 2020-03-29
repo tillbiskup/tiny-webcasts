28e8363cf634b296a72bead532e274402fffe1fb
Till Biskup <till@till-biskup.de>
2020-03-29 11:53:05 +0200

    New command tmpdir; tmpdir extracted as variable

--- webcasts/twc-create-tmpdir.sh
+++ webcasts/twc-create-tmpdir.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+TMPDIR=tmp
+
 # Create temporary directory and copy *wav and *pdf there
-mkdir -p tmp
-cp *wav tmp/
-cp *pdf tmp/
+mkdir -p ${TMPDIR}
+
+# Copy all WAV files not having "raw" in their name
+ls -1 *.wav | grep -v 'raw' | while read filename
+	do cp ${filename} ${TMPDIR}/
+done
+
+#Copy pdf file (presumably the presentation)
+cp *pdf ${TMPDIR}/
